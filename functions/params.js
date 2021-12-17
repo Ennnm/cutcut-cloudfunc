91d311d4d48309c70acae24e2b32a0e89500195f
ennnm <jiaen.1sc4@gmail.com>
2021-12-17 23:11:50 +0800

    working ibm watsons with transcript

--- functions/params.js
+++ functions/params.js
@@ -1,8 +1,9 @@
-export const params = {
-  objectMode: true, // If true, the event handler returns the recognition results exactly as it receives them from the service: as one or more instances of a SpeechRecognitionResults object.
-  contentType: 'application/octet-stream',
+/* eslint quotes: ["error", "double"]*/
+exports.params = {
+  objectMode: true,
+  contentType: "application/octet-stream",
   // model: 'en-US_Multimedia',
-  model: 'en-US_NarrowbandModel',
+  model: "en-US_NarrowbandModel",
   // model: 'en-US_BroadbandModel',
   maxAlternatives: 1,
   interimResults: false,
@@ -12,8 +13,8 @@ export const params = {
   speakerLabels: true,
   processingMetrics: false,
   audioMetrics: false,
-  endOfPhraseSilenceTime: 0.8, //default: 0.8
+  endOfPhraseSilenceTime: 0.8, // default: 0.8
   splitTranscriptAtPhraseEnd: true,
-  speechDetectorSensitivity: 0.5, //default: 0.5, 1.0 suppresses no audio
-  backgroundAudioSuppression: 0.0, //default:0.0, 1.0 suppresses all audio
+  speechDetectorSensitivity: 0.5, // default: 0.5, 1.0 suppresses no audio
+  backgroundAudioSuppression: 0.0, // default:0.0, 1.0 suppresses all audio
 };
